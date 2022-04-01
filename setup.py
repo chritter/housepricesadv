3183c3d174dd0deae6826953a637538bd3efe1c2
chritter <christian.ritter@canada.ca>
2022-04-01 08:45:21 -0400

    restructure project package name, introd pytoml

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='housepricesadv',
+    packages=find_packages(where="housepricesadv"),
+    package_dir={"": "housepricesadv"},  # Optional
     version='0.1.0',
     description='kaggle competition adv house prices modeling',
     author='christian ritter',
